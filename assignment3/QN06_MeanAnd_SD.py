238c04db58a9b99d7f9a684bfaacb46e86baf876
RohilPrajapati <rohilprajapati@gmail.com>
2021-07-09 18:25:51 +0545

    updated ans of QN6

--- assignment3/QN06_MeanAnd_SD.py
+++ assignment3/QN06_MeanAnd_SD.py
@@ -1,11 +1,29 @@
 # Calculate the mean and standard deviation of the following list:
 # Numbers = [1,2,3,5,88,99,55,33,41,52]
 
+from math import *
+
 Numbers = [1,2,3,5,88,99,55,33,41,52]
 
-length = len(Numbers)
+N = len(Numbers)
 total = sum(Numbers)
 
-mean = total/length
+mean = total/N
 
+# output for mean
 print(mean)
+
+# for standar deviation
+
+x_sub_mean=[]
+
+for x in Numbers:
+	x_sub_mean.append(x-mean)
+
+print(x_sub_mean)
+total = sum(x_sub_mean)
+print(total)
+total = total**2
+
+SD= sqrt(total/N)
+print(float(SD))
